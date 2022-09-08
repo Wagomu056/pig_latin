6e540ce6f12ee8e74fbaf6e1602684292baae70b
Takuya Azumaguchi <catchballer@gmail.com>
2022-09-08 22:45:27 +0900

    Use remove function

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ fn main() {
     println!("ai:{}", pig_latin("ai"));
     println!("v:{}", pig_latin("v"));
     println!("we:{}", pig_latin("we"));
+
+    let pig_latin = pig_latin("pig");
+    println!("pig:{}", pig_latin);
 }
 
 fn pig_latin(str: &str) -> String {
@@ -15,7 +18,7 @@ fn pig_latin(str: &str) -> String {
         Some('a') | Some('i') |Some('u') |
         Some('e') | Some('o') => pig_latin_hay(str),
         Some(_) => pig_latin_ay(str),
-        None => String::from(""),
+        None => String::new(),
     }
 }
 
@@ -24,10 +27,10 @@ fn pig_latin_ay(str: &str) -> String {
         return str.to_string();
     }
 
-    str.chars().skip(1).collect::<String>()
-        + "-"
-        + str.chars().nth(0).unwrap().to_string().as_str()
-        + "ay"
+    let mut string = str.to_string();
+    let first_char = string.remove(0);
+
+    string + "-" + &first_char.to_string() + "ay"
 }
 
 fn pig_latin_hay(str: &str) -> String {
